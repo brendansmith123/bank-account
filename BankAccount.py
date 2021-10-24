edb2c1490a17fcc5b2cda7e68a3b67327e7b332a
Brendan Smith <brendansmith@Brendans-MacBook-Air.local>
2021-10-24 17:52:01 -0500

    Second commit

--- BankAccount.py
+++ BankAccount.py
@@ -4,12 +4,18 @@ class BankAccount:
         self.account_number = account_number
         self.balance = balance
 
-
     def print_statement(self):
         print(f"Full name: {self.full_name}, Account number: {self.account_number}, Balance: {self.balance}")
 
+    def deposit(self):
+        amount = str(input("Enter an amount to be deposited: "))
+        self.balance += amount
+        print(f"Amount deposited: {amount} New balance: {self.balance}")
 
-mitchellAccount = BankAccount("Mitchell Trubisky", "58432883", "0")
-mitchellAccount.print_statement()        
+
+
+mitchellAccount = BankAccount("Mitchell Trubisky", "58432883", "0",)
+mitchellAccount.print_statement()  
+mitchellAccount.deposit()      
 
     
